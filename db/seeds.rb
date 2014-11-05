86d497f88317110979f7802497afdc684532c8cf
Przemysław Olesiński <przemek.4mm@gmail.com>
2014-11-05 11:39:18 +0100

    Seeds and turn off registration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Delete User and roles'
+User.delete_all
+Role.delete_all
+
+puts 'Create roles'
+
+Role.create(name: :admin)
+
+
+
+puts 'Create admin'
+
+admin = User.create(email: "admin@example.com", password: "zaq12wsx");
+admin.add_role :admin
+
+# admin.confirmed_at = Time.no.
+# admin.save
+
