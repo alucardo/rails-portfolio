7e4f172aa541083d9f69571093b7b32bd2277c80
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2014-11-07 21:22:43 +0100

    Admin project category

---
+++ app/controllers/admin/project_categories_controller.rb
@@ -0,0 +1,54 @@
+class Admin::ProjectCategoriesController < AdminController 
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @project_categories = ProjectCategory.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = ProjectCategory.new
+  end
+
+  def create
+     @category = ProjectCategory.new(category_params)
+    if @category.save
+      redirect_to admin_project_categories_path, notice: "Dodano kategorie"
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @category.update_attributes(category_params)
+      redirect_to admin_project_categors_path, notice: "Kategoria zapisany"
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy 
+    if @category.delete
+      redirect_to admin_project_categories_path, notice: "Kategoria usuniety"
+    else
+      redirect_to admin_project_categories_path, notice: "Wystapil problem przy usuaniu"
+    end
+  end
+
+  protected
+
+  def set_category
+    @categor = ProjectCategory.friendly.find(params[:id])  
+  end
+
+  def category_params
+    params.require(:project_category).permit(:name)
+  end
+
+end
--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,3 +1,6 @@
 class ProjectCategory < ActiveRecord::Base
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
   has_and_belongs_to_many :projects
 end
